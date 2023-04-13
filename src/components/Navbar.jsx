7eb6ddafdb92060d1dfc8aaa0861585190969fa5
Aaryaman <aaryaman.sh@gmail.com>
2023-04-13 11:32:06 -0700

    Fixing resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -102,7 +102,7 @@ const Navbar = () => {
               </a>
             </li>
             <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
-              <a className="flex justify-between items-center w-full text-gray-300" href="/Aaryaman_Sharma_Resume_April2023.pdf">
+              <a className="flex justify-between items-center w-full text-gray-300" href="/Aaryaman_Sharma_Resume_April2023.pdf" download={true}>
                 Resume <BsFillPersonFill size={30} />
               </a>
             </li>
