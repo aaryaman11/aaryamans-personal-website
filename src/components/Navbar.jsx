a4b671637275251a51c86d1d0e6ffb3bdeb03739
Aaryaman <aaryaman.sh@gmail.com>
2023-04-13 09:46:06 -0700

    Updating navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,14 +20,30 @@ const Navbar = () => {
          <div>
              <ul className='hidden md:flex'> 
                 <li>
-                <Link  to="home"  smooth={true} duration={500}>
-                  Home
-                </Link>
+                  <Link  to="home"  smooth={true} duration={500}>
+                    Home
+                  </Link>
                 </li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+                <li>
+                  <Link  to="about"  smooth={true} duration={500}>
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link  to="skills"  smooth={true} duration={500}>
+                    Skills
+                  </Link>
+                </li>
+                <li>
+                  <Link  to="work"  smooth={true} duration={500}>
+                    Work
+                  </Link>
+                </li>
+                <li>
+                  <Link  to="contact"  smooth={true} duration={500}>
+                    Contact
+                  </Link>
+                </li>
              </ul>
          </div>
 
