8a749c623674580f97f4c4b1253a4dc1a22079bf
Aaryaman <aaryaman.sh@gmail.com>
2023-04-13 10:38:35 -0700

    Integrating react app for github pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,13 +12,13 @@ const Navbar = () => {
     setNav(!nav);
   }
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
-         <div>
+     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+          <div>
             <img src={AS} alt='logo image' style={{width: '80px'}}/>
          </div>
-        {/* Menu*/}
-         <div>
-             <ul className='hidden md:flex'> 
+         {/* Menu*/}
+            
+          <ul className='hidden md:flex'> 
                 <li>
                   <Link  to="home"  smooth={true} duration={500}>
                     Home
@@ -44,8 +44,8 @@ const Navbar = () => {
                     Contact
                   </Link>
                 </li>
-             </ul>
-         </div>
+           </ul>
+      
 
         {/* hamburger menu */}
         <div onClick={handleClick} className="md:hidden z-10">
@@ -56,11 +56,31 @@ const Navbar = () => {
         {/* mobile Icons */}
          <div>
              <ul className={!nav? "hidden": "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
-                <li className="py-6 text-4xl">Home</li>
-                <li className="py-6 text-4xl">About</li>
-                <li className="py-6 text-4xl">Skills</li>
-                <li className="py-6 text-4xl">Work</li>
-                <li className="py-6 text-4xl">Contact</li>
+                <li className="py-6 text-4xl">
+                  <Link onClick={handleClick} to="home"  smooth={true} duration={500}>
+                    Home
+                  </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                  <Link onClick={handleClick} to="about"  smooth={true} duration={500}>
+                    About
+                  </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                  <Link onClick={handleClick} to="skills"  smooth={true} duration={500}>
+                    Skills
+                  </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                  <Link onClick={handleClick} to="work"  smooth={true} duration={500}>
+                    Work
+                  </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                  <Link onClick={handleClick}  to="contact"  smooth={true} duration={500}>
+                    Contact
+                  </Link>
+                </li>
              </ul>
          </div>
         {/*social Icons */}
