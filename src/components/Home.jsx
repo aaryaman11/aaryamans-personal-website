e48e6f651cfeac0ba4835e21e81b5f4f0dc47cb1
Aaryaman <aaryaman.sh@gmail.com>
2023-04-13 23:41:34 -0700

    Fixed resume and about section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {HiArrowNarrowRight} from 'react-icons/hi';
 import {Link} from 'react-scroll';
+import work from '../components/Work';
 
-const Home = () => {
+const Home = ({work}) => {
   return (
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       
@@ -14,7 +15,7 @@ const Home = () => {
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Software Developer.</h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">I'm software developer sepecializing in building web applications. Currently, I am focused on building responsive android applications.</p>
         <div className="flex">
-          <button className=" group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-green-600 hover:border-green-600">View Work
+          <button onClick={work} className=" group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-green-600 hover:border-green-600">View Work
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3" />
             </span>
